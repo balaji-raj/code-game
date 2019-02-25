6e32aad5c0e344298fa3f2450c8972c1c2f46ffd
Karthik n <37000161+karthikneeliyan@users.noreply.github.com>
2019-02-25 16:45:49 +0530

    sample files added.

--- game-app/src/app/app.module.ts
+++ game-app/src/app/app.module.ts
@@ -3,16 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {BoardService} from './app.boardservice.service';
+import {DbconnectionService} from './dbconnection.service'
+import { ChartmakerComponent } from './chartmaker/chartmaker.component'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChartmakerComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [BoardService,DbconnectionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ game-app/src/app/chartmaker/chartmaker.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chartmaker',
+  templateUrl: './chartmaker.component.html',
+  styleUrls: ['./chartmaker.component.css']
+})
+export class ChartmakerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
